5f6cff2f3f9d01a6c24b57bd72d9ae8142a539e0
saefulbarkah <saefulbarkah29062003@gmail.com>
2022-08-10 22:52:44 +0700

    Final build

--- src/app.js
+++ src/app.js
@@ -6,3 +6,12 @@ const closeNav = () => {
   let nav = document.querySelector(".navMobile");
   nav.classList.toggle("translate-y-full");
 };
+
+let navLink = document.querySelectorAll(".menu");
+navLink.forEach((nav) => {
+  nav.addEventListener("click", function () {
+    navLink.forEach((link) => link.classList.remove("active"));
+    this.classList.add("active");
+    console.log(this);
+  });
+});
