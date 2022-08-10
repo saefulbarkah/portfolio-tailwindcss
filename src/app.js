54258657311a5032c15e0335216536817c357ecc
saefulbarkah <saefulbarkah29062003@gmail.com>
2022-08-10 22:00:44 +0700

    Navbar responsive

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const openNav = () => {
+  let nav = document.querySelector(".navMobile");
+  nav.classList.toggle("translate-y-full");
+};
+const closeNav = () => {
+  let nav = document.querySelector(".navMobile");
+  nav.classList.toggle("translate-y-full");
+};
